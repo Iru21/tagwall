1a537ff062686bb5103f8829fa2dd4e2c0243e6d
Mateusz Samsel <m.samsel.113@studms.ug.edu.pl>
2026-02-18 16:26:30 +0100

    Add css import + mono font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,11 @@
 
         <title inertia>{{ config('app.name', 'Tagwall') }}</title>
 
-        @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+
+        @vite(['resources/css/app.css', 'resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
     <body>
