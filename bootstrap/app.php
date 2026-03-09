59e8943c793ab660977000d73d9a164e8e1f2e0d
Mateusz Samsel <m.samsel.113@studms.ug.edu.pl>
2026-03-09 22:07:48 +0100

    Fix type error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,8 +4,6 @@ use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Inertia\Inertia;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -20,7 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
+        $exceptions->respond(function ($response, $exception, $request) {
             $status = $response->getStatusCode();
             if (in_array($status, [404, 403]) ||
                 (!app()->environment(['local', 'testing']) && $status >= 500)) {
